b3306ae2eaf9e41b8dbf30e6e3aaa425d17722c6
Dusan Bjelogrlic <dusanbjelogrlic0@gmail.com>
2025-07-17 21:24:53 +0200

    Sitne promene

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,17 +1,34 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing-module';
 import { provideCharts } from 'ng2-charts';
+
 import { App } from './app';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { RegisterComponent } from './pages/register/register';
 
+import { AuthInterceptor } from './interceptors/auth-interceptor';
+
 @NgModule({
   declarations: [App, DashboardComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [provideBrowserGlobalErrorListeners(), provideCharts()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideCharts(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
--- src/app/interceptors/auth-interceptor.spec.ts
+++ src/app/interceptors/auth-interceptor.spec.ts
@@ -1,17 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { authInterceptor } from './auth-interceptor';
+import { AuthInterceptor } from './auth-interceptor';
+import { AuthService } from '../services/auth.service';
 
-describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+class MockAuthService {
+  getAccessToken(): string | null {
+    return 'mock-token';
+  }
+}
+
+describe('AuthInterceptor', () => {
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+      ]
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should add Authorization header when token exists', () => {
+    http.get('/test').subscribe();
+
+    const req = httpMock.expectOne('/test');
+    expect(req.request.headers.has('Authorization')).toBeTrue();
+    expect(req.request.headers.get('Authorization')).toBe('Bearer mock-token');
   });
 });
